c84f6e39d10c02394468324b7873c6d32f6a54d1
Vincent <vincent@inforvision.nl>
2022-08-26 12:54:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $data = [
     'ip' => $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'],
-    'browser' =>  get_browser(null, true)
+    'browser' => $_SERVER['HTTP_USER_AGENT'] ?? null
 ];
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($data);
